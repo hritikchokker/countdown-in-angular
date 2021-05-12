54127338bf349374e91433231686dda18a07e5b0
hritikchokker <hchokker7@gmail.com>
2021-05-12 23:46:14 +0530

    4 files modified

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,37 +1,85 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { of, Subscription, timer } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
-  selector: "app-countdown",
-  templateUrl: "./countdown.component.html",
-  styleUrls: ["./countdown.component.css"]
+  selector: 'app-countdown',
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.css']
 })
 export class CountdownComponent implements OnInit {
   currDate: Date = new Date();
   years;
   months;
-  hours;
+  hours = 20;
   minutes;
   seconds;
-
+  days = 2;
+  secondsSubscription = new Subscription();
+  hoursSubscription = new Subscription();
+  minutesSubscription = new Subscription();
   constructor() {
-    this.currDate.setDate(1);
-    this.currDate.setHours(2);
-    this.currDate.setSeconds(10);
-    this.currDate.setMinutes(20);
-    window.clearInterval();
-    window.clearTimeout();
-    window.clearTimeout();
-    // setInterval(() => {
-    //   const currMins = this.currDate.getMinutes() - 1;
-    //   const currSeconds = this.currDate.getSeconds() - 1;
-    //   if (this.currDate.getSeconds() === 0) {
-    //     this.currDate.setSeconds(60);
-    //     this.currDate.setMinutes(currMins);
-    //   } else {
-    //     this.currDate.setSeconds(currSeconds);
+    this.manageSeconds(60);
+    this.manageMinutes(1);
+    this.manageHours(20);
+    // // or
+    // Rx.Observable
+    //   .range(0, start + 1)
+    //   .map(i => start - i)
+    //   .subscribe(i => console.log(i));
     //   }
-    //   // this.currDate = new Date();
-    // }, 1000);
+  }
+  manageMinutes(start: number) {
+    this.minutesSubscription.add(
+      timer(100, 60000)
+        .pipe(
+          map(i => start - i),
+          take(start + 1)
+        )
+        .subscribe(i => {
+          if (i == 0) {
+            if (this.hours === 0) {
+              this.minutesSubscription.unsubscribe();
+            }
+            if (this.hours > 0) {
+              this.hours--;
+              this.manageMinutes(60);
+            }
+          }
+          this.minutes = i;
+        })
+    );
+  }
+  manageSeconds(start = 60) {
+    this.secondsSubscription.add(
+      timer(100, 1000)
+        .pipe(
+          map(i => start - i),
+          take(start + 1)
+        )
+        .subscribe(i => {
+          if (this.minutes === 0 && this.hours === 0) {
+            this.secondsSubscription.unsubscribe();
+          }
+          if (i == 0) {
+            this.manageSeconds();
+          }
+          this.seconds = i;
+        })
+    );
+  }
+
+  manageHours(start = 12) {
+    // this.hoursSubscription.add(
+    //   timer(100, 6000)
+    //     .pipe(
+    //       map(i => start - i),
+    //       take(start + 1)
+    //     )
+    //     .subscribe(i => {
+    //       this.hours = i;
+    //     })
+    // );
   }
   ngOnInit() {}
 }
