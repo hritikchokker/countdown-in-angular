bd4209e888c1ff2727d667c78a2345a128d44049
hritikchokker <hchokker7@gmail.com>
2021-05-13 18:01:27 +0530

    1 file modified

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -11,24 +11,16 @@ export class CountdownComponent implements OnInit {
   currDate: Date = new Date();
   years;
   months;
-  hours = 24;
-  minutes = 59;
-  seconds = 60;
-  days = 2;
+  hours = 1;
+  minutes = 10;
+  seconds = 10;
+  days = 10;
   secondsSubscription = new Subscription();
   hoursSubscription = new Subscription();
   minutesSubscription = new Subscription();
-  constructor() {
-    // // or
-    // Rx.Observable
-    //   .range(0, start + 1)
-    //   .map(i => start - i)
-    //   .subscribe(i => console.log(i));
-    //   }
-  }
-
+  constructor() {}
   ngOnInit(): void {
-    // this.manageCountDown();
+    this.manageCountDown();
   }
   manageCountDown(start = 59) {
     this.secondsSubscription.add(
@@ -41,89 +33,37 @@ export class CountdownComponent implements OnInit {
           this.seconds = data;
           if (this.days === 0 && this.hours === 0 && this.minutes === 0) {
             this.secondsSubscription.unsubscribe();
-          }
-          if (this.minutes === 0) {
-            this.minutes = 59;
-          } else {
-            this.minutes--;
-          }
-          if (this.hours === 0) {
-            this.hours = 23;
-          } else {
-            this.hours--;
-          }
-          if (this.days === 0) {
-            this.manageCountDown();
             return;
           }
           if (this.seconds === 0) {
-            this.manageCountDown();
+            if (this.days >= 1 && this.hours === 0 && this.minutes == 0) {
+              this.hours = 23;
+              this.minutes = 59;
+              this.days--;
+              this.manageCountDown();
+              return;
+            }
+            if (this.minutes === 0 && this.hours > 0) {
+              this.hours--;
+              this.minutes = 59;
+              this.manageCountDown();
+              return;
+            }
+            if (this.minutes > 0) {
+              this.minutes--;
+              this.manageCountDown();
+              return;
+            }
+            // if (this.hours === 0 && this.minutes === 0 && this.days > 0) {
+            //   this.days--;
+            //   this.hours = 23;
+            //   this.minutes = 59;
+            //   this.manageCountDown();
+            //   return;
+            // }
+            // this.manageCountDown();
           }
         })
     );
   }
-  // manageMinutes(start: number) {
-  //   this.minutesSubscription.add(
-  //     timer(100, 60000)
-  //       .pipe(
-  //         map(i => start - i),
-  //         take(start + 1)
-  //       )
-  //       .subscribe(i => {
-  //         if (i == 0) {
-  //           if (this.hours === 0) {
-  //             if (this.days > 0) {
-  //               this.hours = 24;
-  //               this.manageMinutes(60);
-  //             } else {
-  //               this.minutesSubscription.unsubscribe();
-  //             }
-  //           }
-  //           if (this.hours > 0) {
-  //             this.hours--;
-  //             this.manageMinutes(60);
-  //           }
-  //         }
-  //         this.minutes = i;
-  //       })
-  //   );
-  // }
-  // manageSeconds(start = 60) {
-  //   this.secondsSubscription.add(
-  //     timer(100, 1000)
-  //       .pipe(
-  //         map(i => start - i),
-  //         take(start + 1)
-  //       )
-  //       .subscribe(i => {
-  //         if (this.minutes === 0 && this.hours === 0) {
-  //           if (this.days > 0) {
-  //             this.days--;
-  //             this.hours = 24;
-  //             this.minutes = 60;
-  //             return;
-  //           }
-  //           this.secondsSubscription.unsubscribe();
-  //         }
-  //         if (i == 0) {
-  //           // this.minutes = 60;
-  //           this.manageSeconds();
-  //         }
-  //         this.seconds = i;
-  //       })
-  //   );
-  // }
-
-  // manageHours(start = 12) {
-  //   // this.hoursSubscription.add(
-  //   //   timer(100, 6000)
-  //   //     .pipe(
-  //   //       map(i => start - i),
-  //   //       take(start + 1)
-  //   //     )
-  //   //     .subscribe(i => {
-  //   //       this.hours = i;
-  //   //     })
-  //   // );
-  // }
 }
